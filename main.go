241fb760a30d10365ba2e2b3601b6eb61ebcdb50
Henocega <ssafonov.dev@gmail.com>
2026-03-01 21:04:39 +0300

    Add main and get token

---
+++ main.go
@@ -0,0 +1,26 @@
+package limoncello_bot
+
+import (
+	"flag"
+	"log"
+)
+
+func main() {
+	t := mustToken()
+}
+
+func mustToken() string {
+	token := flag.String(
+		"tg-bot-token",
+		"",
+		"token for access to telegram bot",
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *token
+}
